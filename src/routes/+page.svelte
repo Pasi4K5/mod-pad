dff324f381ce6020ef33c3d43a77a112cc8db60e
Pascal Pomper <pascal.pomper03@gmail.com>
2025-06-29 01:50:20 +0200

    Fix timestamps not being clickable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,7 +45,11 @@
 		return text.replace(
 			/(\d{2}:\d{2}:\d{3}( \(\d+(,\d+)*\))?)/g,
 			match => {
-				return `<a href="osu://edit/${encodeURI(match)}" class="text-blue-200 hover:underline pointer-events-auto font-bold bg-gray-900 rounded-sm">${match}</a>`;
+				return '<a '
+					+ `href="osu://edit/${encodeURI(match)}" `
+					+ 'class="z-2 text-blue-200 hover:underline pointer-events-auto font-bold rounded-sm" '
+					+ 'style="background-color: rgba(0, 0, 0, 0.333);" '
+					+ `>${match}</a>`;
 			}
 		);
 	}
@@ -57,19 +61,20 @@
 
 <div class="m-2 relative">
 
-	<!-- Overlay -->
-	<div
-		class="absolute z-0 w-full pointer-events-none wrap-break-word"
-	>
-		<!-- eslint-disable svelte/no-at-html-tags -->
-		{@html overlayHtml}
-	</div>
-
 	<!-- Editor -->
 	<textarea
 		bind:this={editor}
 		oninput={handleInput}
-		class="absolute z-1 outline-none text-transparent caret-white w-full resize-none"
+		onblur={() => setTimeout(() => editor.focus(), 0)}
+		class="absolute outline-none text-transparent caret-white w-full resize-none"
 	></textarea>
+
+	<!-- Overlay -->
+	<div
+		class="absolute w-full pointer-events-none wrap-break-word"
+	>
+		<!-- eslint-disable svelte/no-at-html-tags -->
+		{@html overlayHtml}
+	</div>
 
 </div>
