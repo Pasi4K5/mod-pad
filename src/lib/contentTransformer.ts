ac4f5137ffcaad3134890246dfcd01bd792ed859
Pascal Pomper <pascal.pomper03@gmail.com>
2025-06-29 12:53:22 +0200

    Update prettier configuration and extract functions

---
+++ src/lib/contentTransformer.ts
@@ -0,0 +1,27 @@
+import { sanitize } from '$lib/stringUtil';
+
+export function transform(text: string): string {
+    text = sanitize(text);
+    text = replaceOsuTimestamps(text);
+    text = replaceTodos(text);
+
+    return text;
+}
+
+function replaceOsuTimestamps(text: string): string {
+    return text.replace(/(\d{2}:\d{2}:\d{3}( \(\d+(,\d+)*\))?)/g, (match) => {
+        return (
+            '<a ' +
+            `href="osu://edit/${encodeURI(match)}" ` +
+            'class="z-2 text-blue-200 bg-black/33 hover:underline pointer-events-auto font-bold rounded-sm" ' +
+            `>${match}</a>`
+        );
+    });
+}
+
+function replaceTodos(text: string): string {
+    return text.replace(
+        /(\bTODO\b.*)/g,
+        "<span class='font-bold text-amber-600 bg-amber-600/20 rounded-sm'>$1</span>",
+    );
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,87 +1,64 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
+    import { transform } from '$lib/contentTransformer';
 
-	let editor: HTMLTextAreaElement;
-	let overlayHtml = "";
+    let editor: HTMLTextAreaElement;
+    let overlayHtml = '';
 
-	onMount(() => {
-		editor.focus();
-		rerender();
-	});
+    onMount(() => {
+        editor.focus();
+        rerender();
+    });
 
-	function normalizeEditorContent(): void {
-		editor.value = editor.value.replace(/\t/g, "  ");
-		updateEditorHeight();
-	}
+    function normalizeEditorContent(): void {
+        editor.value = editor.value.replace(/\t/g, '  ');
+        updateEditorHeight();
+    }
 
-	function updateEditorHeight(): void {
-		editor.style.height = `${editor.scrollHeight}px`;
-	}
+    function updateEditorHeight(): void {
+        editor.style.height = `${editor.scrollHeight}px`;
+    }
 
-	function rerender(): void {
-		normalizeEditorContent();
+    function rerender(): void {
+        normalizeEditorContent();
 
-		let text = sanitize(editor.value);
-		text = replaceOsuTimestamps(text);
-		text = replaceTodos(text);
-		text = text.replaceAll("  ", "&nbsp;&nbsp;")
-			.replaceAll("\n", "&nbsp;\n")
-			+ "&nbsp;";
-		const lines = text.split("\n");
+        let text = editor.value;
+        text = text.replaceAll("  ", "&nbsp;&nbsp;")
+            .replace(/^ /g, "&nbsp;")
+            .replaceAll("\n", "&nbsp;\n");
+        text = transform(text);
+        const lines = text.split('\n');
 
-		let html = "";
+        let html = '';
 
-		let i: number;
-		for (i = 0; i < lines.length; i++) {
-			const line = lines[i];
+        let i: number;
+        for (i = 0; i < lines.length; i++) {
+            const line = lines[i];
 
-			html += `<div>${line}</div><div class="h-[1px] mb-[-1px] bg-gray-700"></div>`;
-		}
+            html += `<div>${line}</div><div class="h-[1px] mb-[-1px] bg-gray-700"></div>`;
+        }
 
-		overlayHtml = html;
-	}
-
-	function replaceOsuTimestamps(text: string): string {
-		return text.replace(
-			/(\d{2}:\d{2}:\d{3}( \(\d+(,\d+)*\))?)/g,
-			match => {
-				return '<a '
-					+ `href="osu://edit/${encodeURI(match)}" `
-					+ 'class="z-2 text-blue-200 bg-black/33 hover:underline pointer-events-auto font-bold rounded-sm" '
-					+ `>${match}</a>`;
-			}
-		);
-	}
-
-	function replaceTodos(text: string): string {
-		return text.replace(
-			/(\bTODO\b.*)/g,
-			"<span class='font-bold text-amber-600 bg-amber-600/20 rounded-sm'>$1</span>"
-		);
-	}
-
-	function sanitize(text: string): string {
-		return text.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-	}
+        overlayHtml = html;
+    }
 </script>
 
 <div class="m-4 relative">
 
-	<!-- Editor -->
-	<textarea
-		bind:this={editor}
-		oninput={rerender}
-		onblur={() => setTimeout(() => editor.focus(), 0)}
-		onpaste={normalizeEditorContent}
-		class="absolute outline-none text-transparent caret-white w-full resize-none"
-	></textarea>
+    <!-- Editor -->
+    <textarea
+        bind:this={editor}
+        oninput={rerender}
+        onblur={() => setTimeout(() => editor.focus(), 0)}
+        onpaste={normalizeEditorContent}
+        class="absolute outline-none text-transparent caret-white w-full resize-none"
+    ></textarea>
 
-	<!-- Overlay -->
-	<div
-		class="absolute w-full pointer-events-none wrap-break-word pb-4"
-	>
-		<!-- eslint-disable svelte/no-at-html-tags -->
-		{@html overlayHtml}
-	</div>
+    <!-- Overlay -->
+    <div
+        class="absolute w-full pointer-events-none wrap-break-word pb-4"
+    >
+        <!-- eslint-disable svelte/no-at-html-tags -->
+        {@html overlayHtml}
+    </div>
 
 </div>
